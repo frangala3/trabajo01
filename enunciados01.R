04f9b67d86a1e7d789e89df1a86c74e3dd58a33e
frangala <fgalanaranjo4.fjgne@gmail.com>
2024-10-07 22:59:44 +0200

    Primer enunciado

---
+++ enunciados01.R
@@ -0,0 +1,11 @@
+source("~/Fran/Curso 24-25/1 TD/trabajo01/teoriadecision_funciones_incertidumbre.R")
+
+# Enunciados de los problemas
+
+#PROBLEMA 1: Sea la tabla de decisión con 4 estados de la naturaleza y 5
+#alternativas de la siguiente forma:
+
+X1=crea.tablaX(c(6,9,4,6,5,5,6,5,6,6,6,8,6,8,6,6,5,8,9,15),numalternativas = 5,numestados = 4)
+
+#Resolverla tanto para situación favorable como desfavorable, con cada uno de los 
+#criterios por separado.
